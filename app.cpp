df65a982a9103451e66bd00bb3d63c0e22c2b0f7
dookie182 <franckot@gmail.com>
2023-05-31 17:32:53 +0200

    Working Map With Optimizations

--- app.cpp
+++ app.cpp
@@ -7,15 +7,13 @@
 #include <string_view>
 #include <sstream>
 #include <future>
+#include <queue>
 #include <thread>
 #include "utimer.cpp"
 #include "./include/ThreadPool.hpp"
 #include "./include/SafeQueue.h"
 #include <sstream>
 #include <unordered_map>
-
-
-
 
 
 using namespace std;
@@ -59,86 +57,44 @@ void print_map(std::string_view comment, const unordered_map<std::string, int>& 
  
 // C++11 alternative:
 //  for (const auto& n : m)
-//      std::cout << n.first << " = " << n.second << "; ";
-//
-// C++98 alternative
-//  for (std::map<std::string, int>::const_iterator it = m.begin(); it != m.end(); it++)
-//      std::cout << it->first << " = " << it->second << "; ";
- 
+//      std::cout << n.first << " = " << n.second << "; "; 
     std::cout << '\n';
 }
 
 void read(string line, unordered_map<string, int>& m) {
 
-    istringstream input2;
-    input2.str(line);
-    for (string elem; getline(input2,elem, ' '); ){
-        cout << elem << '\n';
+    istringstream input;
+    input.str(line);
+    for (string elem;  getline(input,elem, ' ');){
 		m[elem] = m[elem] + 1;
 		}
 
 }
 
-  
-
-
-
-
-void loadText(){
-  
-	string fname = "./data/asciiText.txt";
-
-	vector<vector<string>> content;
-	vector<string> row;
-	string line, word;
- 
-	fstream file (fname, ios::in);
-	if(file.is_open())
-	{
-		while(getline(file, line))
-		{
-			row.clear();
- 
-			stringstream str(line);
- 
-			while(getline(str, word, ' '))
-				row.push_back(word);
-			content.push_back(row);
-		}
-	}
-	else
-		cout<<"Could not open the file\n";
- 
-	for(int i=0;i<content.size();i++)
-	{
-		for(int j=0;j<content[i].size();j++)
-		{
-			cout<<content[i][j]<<" ";
-		}
-		cout<<"\n";
-	}
-}
-
 int main(){
-	utimer t0("Huffman Encoding");
+	utimer t0("Complete Execution");
 	vector<int> v;
 	string fname = "./data/asciiText.txt";
 	string line;
 	fstream file (fname, ios::in);
+	priority_queue<int> q1;
 
-	unordered_map<string, int> m{};
+	unordered_map<string, int> m;
 
 
-	ThreadPool pool(10);
+	ThreadPool pool(64);
 	pool.init();
+	utimer t1("Huffman Encoding");
 
 	while(!file.eof()){
 		getline(file,line);
 		auto future = pool.submit(read,line, ref(m));
 		future.get();
 	}
-	print_map("Map:",m);
+	
+	//print_map("Map:",m);
 	pool.shutdown();
+	file.close();
 
 return 0;
 }
