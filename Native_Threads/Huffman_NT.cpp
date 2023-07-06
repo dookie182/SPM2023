6b8ba51d148c336fdb4ef19bdc9dfe7c72c11ce9
dookie182 <franckot@gmail.com>
2023-07-06 11:20:02 +0200

    New NT implementation - NO THP

--- Native_Threads/Huffman_NT.cpp
+++ Native_Threads/Huffman_NT.cpp
@@ -145,38 +145,48 @@ void readFile(string line, unordered_map<char, int>* m) {
 	}
 
 	// Storing Data in original Map;
+	mutual_exclusion.lock();
 	for (const auto &pair: temp) {
-        mutual_exclusion.lock();
 		(*m)[pair.first] = (*m)[pair.first] + pair.second;
-        mutual_exclusion.unlock();
+	}
+	mutual_exclusion.unlock();
+
+
+
+}
+
+void encodeFile(string line, unordered_map<char, string> huffmanMap, vector<string> *partial_writes, int index) {
+
+	string to_write,front;
+
+	//string tmp = line.substr(index*chunk_size,chunk_size);
+	// vector<string> tmp;
+
+	// for (char elem: line){
+	// 	tmp.push_back(huffmanMap[elem]);
+	// }
+
+	for (auto elem : line){
+		to_write += huffmanMap[elem];
+		if (to_write.size() > 8){
+			front = to_write.substr(0,8);
+			bitset<8> c(front);			
+			(*partial_writes)[index] += static_cast<char>(c.to_ulong());
+			to_write = to_write.substr(8, to_write.size() - front.size());
+	}
 	}
 
-}
-
-void writeFile(string line, unordered_map<char, string> huffmanMap,vector <char> *partial_writes) {
-
-	string output,tmp;
-	vector <string> tmp_str;
-
-    
-	auto dash_fold = [](std::string a, string b)
-    {
-        return move(a) + move(b);
-    };
-
-	// for(char elem : line){
-	// 	tmp_str.push_back(huffmanMap[elem]);
-	// 	if(tmp_str.size() == 8){
-	// 			tmp = accumulate(std::next(tmp_str.begin()), tmp_str.begin() + 8,
-    //                                 (tmp_str[0]), // start with first element
-    //                                 dash_fold);
-	// 			bitset<8> c(tmp);
-	// 			(*partial_writes).insert((*partial_writes).begin() + index,static_cast<char>(c.to_ulong()));
-	// 			index++;
-	// 			tmp_str.clear();
-	// 		}
+	// for(char elem : tmp){
+	// 	to_write += huffmanMap[elem];
+	// 	if (to_write.size() > 8){
+	// 		front = to_write.substr(0,8);
+	// 		bitset<8> c(front);			
+	// 		(*partial_writes)[index] += static_cast<char>(c.to_ulong());
+	// 		to_write = to_write.substr(8, to_write.size() - front.size());
+	// }
 	// }
 }
+
 
 
 void clean_memory (Node* root){
@@ -191,8 +201,9 @@ void clean_memory (Node* root){
 }
 
 void start_exec(int nw, string fname, string compressedFname){
-    vector<future<void>> future_arr;
-    vector<future<void>> future_arr2;
+	vector<thread> tids;
+	vector<thread> tids2;
+	vector<string> partial_writes(nw);
 
     // File Management
     fstream file (fname, ios::in);
@@ -201,9 +212,7 @@ void start_exec(int nw, string fname, string compressedFname){
 	file.seekg(0);
     int file_lenght = file.tellg();
 	int begin = 0;
-    int chunk_size = file_lenght/nw;
     string line, tmp, to_send;
-    vector <char> partial_writes;
 
 	priority_queue<Node*, vector<Node*>,compare> queue;
 	unordered_map<char, int> m;
@@ -214,15 +223,11 @@ void start_exec(int nw, string fname, string compressedFname){
 	cout << "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_" << endl;
 	cout << "Working with " << nw << " workers " << endl; 
 
-    // ThreadPool Initialization
-	ThreadPool pool(nw);
-	pool.init();
-
     // Reading File
     {utimer t0("File Reading:");
         do{
             getline(file,line);
-			tmp += line;
+			tmp += line;	
 		}while(!file.eof());   
     }
 
@@ -230,58 +235,128 @@ void start_exec(int nw, string fname, string compressedFname){
 
 		int chunk_size = tmp.size() / nw;    
 
-        for (int i = 0; i < nw; i++){
-			to_send = tmp.substr(begin,chunk_size);
-            future_arr.push_back(pool.submit(readFile, to_send, &m));
-			begin += chunk_size;
+        while(begin < tmp.size()){
+			if (tmp.size() - begin > chunk_size){
+				to_send = tmp.substr(begin,chunk_size);
+				tids.push_back(thread(readFile,to_send, &m));
+				begin += chunk_size;
+			}else{
+				to_send = tmp.substr(begin,tmp.size() - begin );
+				tids.push_back(thread(readFile,to_send, &m));
+				begin = tmp.size();
+			}
         }
-
-		for (auto& elem : future_arr){
-			elem.get();
+		for (auto& elem : tids){
+			elem.join();
 		}
+
     }
+
 	{utimer t2("Building & Encoding Huffman Tree");
 		huffmanMap = huffmanTreeBuilder(m,ref(queue),root);
 	}
 
-	{utimer t3("Writing compressed File");
+	
 		begin = 0;
-        for (int i = 0; i < nw; i++){
-			to_send = tmp.substr(begin,chunk_size);
-            future_arr2.push_back(pool.submit(writeFile,line, huffmanMap, &partial_writes));
+		int length = tmp.length();
+		int chunk_size = length / nw;    
+		int j = 0;
+        for(j = 0; j < nw; j++){
+			to_send = tmp.substr(0,chunk_size);
+			tids2.push_back(thread(encodeFile,to_send,huffmanMap,&partial_writes,j));
 			begin += chunk_size;
-        }
+		}
+
+		// 		begin = 0;
+		// int length = tmp.length();
+		// int chunk_size = length / nw;    
+		// int j = 0;
+        // for(j = 0; j < nw; j++){
+		// 	tids2.push_back(thread(encodeFile,tmp,huffmanMap,&partial_writes,j,chunk_size));
+		// }
+
+		for (auto& elem : tids2){
+			elem.join();
+		}
+	
+	{utimer t4("Writing compressed File");
+		for (auto elem : partial_writes){
+			compressed_file << elem;
+		}
 	}
+
+	// Closing Files 
 	file.close();
 	compressed_file.close();
 
-	pool.shutdown();
+}
+
+void foo(){
+
+	long usecs;
+
+	{utimer body("Body Exec", &usecs);
+	int tmp = 2;
+
+
+	for(int i = 0; i < 10000; i++){
+		tmp*= tmp;
+	}
+
+	}
+
 }
 
 int main(int argc, char * argv[]){
 	
 	string fname = (argc > 1 ? argv[1] : "../data/dataset.txt");  // Input File Name
 	string compressedFname = (argc > 1 ? argv[2] : "../data/dataset_compressed_NT.txt");  // Output File Name
-	//nt nw = (argc > 3 ? atoi(argv[3]) : 32);   // par degree
-	fstream log("../data/log.txt",fstream::app);
+	fstream log("../data/log.txt",fstream::app); // Statistics File
 
-	long usecs;
+	long usecs, usecs2;
 	long time_seq;
 	double speedup;
+	vector<thread> tids_ex;
+	vector<long> thread_timings;
+
+
+	for (int i = 1; i < 64; i*=2)
+			// Timings about Thread Generation
+		{
+		utimer threads("Fork of threads", &usecs2);
+
+			for (int j = 0; j < i; j++){
+					tids_ex.push_back(thread(foo));			
+				}
+
+			for (auto& tid : tids_ex){
+				if(tid.joinable()){
+					tid.join();
+				}
+			}
+		}
+
+
+
+
 
 	for (int i = 1; i <= 64; i*=2){
+
 		{
-			utimer t0("End:", &usecs);
+			utimer t0("Total Execution:", &usecs);
+			// Starting Execution with i Workers
 			start_exec(i,fname,compressedFname);
 		}
 		
 		if (i == 1){
+			// Storing total sequential time computation
 			time_seq = usecs;
 		}
 		if(usecs != 0) {
+			// Evaluating SpeedUp
 			speedup = time_seq / (double)usecs;
-			log << "SpeedUp with " << i << " Threads:"<< speedup << endl;
-			cout << "SpeedUp with " << i << " Threads:"<< speedup << endl;
+			log << "Scalability with " << i << " Threads:"<< speedup << endl;
+			cout << "Scalability with " << i << " Threads:"<< speedup << endl;
 
 		}
 	}
